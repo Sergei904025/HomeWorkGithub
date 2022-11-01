4d162606e7a4f6e3a967eab4aab9125d76cf04c0
Sergei904025 <105377873+Sergei904025@users.noreply.github.com>
2022-11-01 13:21:02 +0600

    Initial commit

---
+++ src/main/java/dz1/HomeWorkApp.java
@@ -0,0 +1,70 @@
+package dz1;
+
+public class HomeWorkApp {
+    public static void main(String[] args) {
+        printThreeWords();
+        printcheckSumSign();
+        printColor();
+        compareNumbers();
+    }
+
+
+    private static void printThreeWords() {
+        System.out.println("orange");
+        System.out.println("banana");
+        System.out.println("apple");
+    }
+
+    private static void printcheckSumSign() {
+        int varA = 4;
+        int varB = 5;
+        System.out.println(varA + varB);
+
+
+        if (varA + varB >= 0) {
+            System.out.println("сумма положительная");
+        }
+
+        if (varA + varB < 0) {
+            System.out.println("сумма отрицательная");
+        }
+
+
+    }
+
+    private static void printColor() {
+        int value = 200;
+
+        if (value <= 0) {
+
+            System.out.println("красный");
+        }
+        if (value > 0 && value <= 100) {
+
+            System.out.println("желтый");
+        }
+        if (value > 100) {
+
+            System.out.println("зеленый");
+        }
+
+
+    }
+
+    private static void compareNumbers() {
+
+
+        int A = 3;
+        int B = 5;
+        if (A >= B) {
+
+            System.out.println("A >= B");
+        }
+        if (A < B) {
+
+            System.out.println("A < B");
+        }
+
+
+    }
+}
